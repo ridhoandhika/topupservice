bc85eebcdaccd27943969202026f970d5343ff8a
ridhoandhika <ridhoandhika74@gmail.com>
2025-07-10 22:04:59 +0700

    allow all origin

--- main.go
+++ main.go
@@ -23,9 +23,9 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000, http://localhost:5173, http://localhost:8081, http://localhost:8080", // Membolehkan domain tertentu
-		AllowMethods: "GET,POST,PUT,DELETE,PATCH",                                                                  // Metode HTTP yang diizinkan
-		AllowHeaders: "Origin, Content-Type, Accept, Authorization",                                                // Header yang diizinkan
+		AllowOrigins: "*",                                           // "http://localhost:3000, http://localhost:5173, http://localhost:8081, http://localhost:8080", // Membolehkan domain tertentu
+		AllowMethods: "GET,POST,PUT,DELETE,PATCH",                   // Metode HTTP yang diizinkan
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization", // Header yang diizinkan
 	}))
 
 	app.Use(logger.New(logger.Config{
